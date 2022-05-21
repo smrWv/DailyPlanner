d6e787fdf5f22062c7056319abad5f2f8849e118
Vasilev Sergey <vsmbx09@gmail.com>
2022-05-21 14:27:23 +0300

    feat: create project structure

---
+++ src/test/java/com/smrwv/dailyplanner/DailyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smrwv.dailyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
